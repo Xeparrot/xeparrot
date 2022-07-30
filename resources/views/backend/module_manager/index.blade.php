5f19aad58e5dee8547b7fe920d8816976fa2ef4b
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-07-31 02:12:17 +0530

    Resource Added

---
+++ resources/views/backend/module_manager/index.blade.php
@@ -0,0 +1,11 @@
+@extends('backend.layouts.app')
+
+@section('title', __('Module Explorer'))
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+
+        </div>
+    </div>
+@endsection
