0f1f31bbd3a4def9b9ae0cfa9cea5026319185b5
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-07-31 02:13:07 +0530

    Menu Updateds

--- menu/menu.php
+++ menu/menu.php
@@ -5,7 +5,7 @@ Menu::create('sidebar_core', function($menu) {
     // URL, Title, Attributes
 
     $menu->dropdown('Module Manager', function ($sub) {
-        $sub->url('admin/blog', 'Explorer');
+        $sub->url( url('admin/module-explorer'), 'Explorer');
         $sub->url('admin/blog', 'Wishlist');
         $sub->url('admin/blog', 'Installed Modules');
     }, [
