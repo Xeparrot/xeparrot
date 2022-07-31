bcb412794276d98adc51a7b980148755ee5818b7
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-07-31 17:53:00 +0530

    Xeparrot Updates

--- resources/views/backend/customers/index.blade.php
+++ resources/views/backend/customers/index.blade.php
@@ -3,26 +3,7 @@
 @section('title', __('Dashboard'))
 
 @section('content')
-    <x-backend.card>
-        <x-slot name="header">
-            @lang('User Management')
-        </x-slot>
 
-        @if ($logged_in_user->hasAllAccess())
-            <x-slot name="headerActions">
-                <x-utils.link
-                        icon="c-icon cil-plus"
-                        class="card-header-action"
-                        :href="route('admin.auth.user.create')"
-                        :text="__('Create User')"
-                />
-            </x-slot>
-        @endif
-
-        <x-slot name="body">
-            <livewire:backend.users-table />
-        </x-slot>
-    </x-backend.card>
 @endsection
 
 
