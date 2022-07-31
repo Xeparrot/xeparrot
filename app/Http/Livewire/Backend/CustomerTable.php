6af923a836f6852712f1aff2fc3dbe39eafc3695
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-07-31 07:28:41 +0530

    Customer Tabales Engima

---
+++ app/Http/Livewire/Backend/CustomerTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Livewire\Backend;
+
+use App\Domains\Auth\Models\User;
+use App\Models\Models\CustomerDetails;
+use Livewire\Component;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+
+class CustomerTable extends Component
+{
+    public function query(): Builder
+    {
+        return User::with('permissions:id,name,description')
+            ->withCount('users')
+            ->when($this->getFilter('search'), fn ($query, $term) => $query->search($term));
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make(__('Type'))
+                ->sortable(),
+            Column::make(__('Name'))
+                ->sortable(),
+            Column::make(__('Permissions')),
+            Column::make(__('Number of Users'), 'users_count')
+                ->sortable(),
+            Column::make(__('Actions')),
+        ];
+    }
+
+    public function rowView(): string
+    {
+        return 'backend.customers.includes.row';
+    }
+}
--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\DashboardController;
 use Tabuna\Breadcrumbs\Trail;
 use App\Http\Controllers\Backend\SettingsController;
 use App\Http\Controllers\Backend\ModuleExplorer;
+use App\Http\Controllers\Backend\CustomerController;
 // All route names are prefixed with 'admin.'.
 Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', [DashboardController::class, 'index'])
@@ -15,6 +16,8 @@ Route::get('dashboard', [DashboardController::class, 'index'])
 
 Route::get('settings/{setting_type}',[SettingsController::class,'index'])->name('settings.index');
 Route::post('settings-store',[SettingsController::class,'store'])->name('settings.store');
+
+Route::get('customers',[CustomerController::class,'index'])->name('customer.index');
 
 Route::get('module-explorer',[ModuleExplorer::class,'explorer'])->name('module.explorer');
 Route::post('module-download',[ModuleExplorer::class,'module_download'])->name('module.download');
