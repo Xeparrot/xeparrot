48261c959640f2231761edffc22928310b6cc52a
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-08-11 22:52:16 +0530

    App Blades Functions

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -7,11 +7,51 @@
     <title>{{ appName() }} | @yield('title')</title>
     <meta name="description" content="@yield('meta_descrsiption', appName())">
     <meta name="author" content="@yield('meta_author', 'Sanjaya Senevirathne')">
+
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="app-url" content="{{config('app.url')}}">
+    <meta name="file-base-url" content="{{config('app.url')}}/public">
+
     @yield('meta')
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
     @stack('before-styles')
     <link href="{{ mix('css/backend.css') }}" rel="stylesheet">
     <livewire:styles />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="{{url('css/aiz-core.css')}}">
+    <link rel="stylesheet" href="{{url('css/vendors.css')}}">
+
+    <script>
+        var AIZ = AIZ || {};
+        AIZ.local = {
+            nothing_selected: 'Nothing selected',
+            nothing_found: 'Nothing found',
+            choose_file: 'Choose file',
+            file_selected: 'File selected',
+            files_selected: 'Files selected',
+            add_more_files: 'Add more files',
+            adding_more_files: 'Adding more files',
+            drop_files_here_paste_or: 'Drop files here, paste or',
+            browse: 'Browse',
+            upload_complete: 'Upload complete',
+            upload_paused: 'Upload paused',
+            resume_upload: 'Resume upload',
+            pause_upload: 'Pause upload',
+            retry_upload: 'Retry upload',
+            cancel_upload: 'Cancel upload',
+            uploading: 'Uploading',
+            processing: 'Processing',
+            complete: 'Complete',
+            file: 'File',
+            files: 'Files',
+        }
+    </script>
+
     @stack('after-styles')
 </head>
 <body class="c-app">
@@ -38,9 +78,19 @@
     </div><!--c-wrapper-->
 
     @stack('before-scripts')
+
     <script src="{{ mix('js/manifest.js') }}"></script>
     <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/backend.js') }}"></script>
+
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/responsive.bootstrap4.min.js"></script>
+
+    <script src="{{url('js/vendors.js')}}"></script>
+    <script src="{{url('js/aiz-core.js')}}"></script>
+
     <livewire:scripts />
     @stack('after-scripts')
 </body>
