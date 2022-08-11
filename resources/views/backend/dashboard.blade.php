53bda97ca9eaa9f59a9271d1f4104f8e483045ec
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-08-11 22:52:33 +0530

    Dashboard Blade Card Functions

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -3,21 +3,42 @@
 @section('title', __('Dashboard'))
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
+
+    <div class="card">
+        <div class="card-body">
+            <form action="" method="post" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body">
 
 
+
+                                <div class="form-group">
+                                    <label>Image
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group" data-toggle="aizuploader" data-type="image">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text bg-soft-secondary font-weight-medium">Browse</div>
+                                        </div>
+                                        <div class="form-control file-amount">Choose File</div>
+                                        <input type="hidden" name="image" class="selected-files" >
+                                    </div>
+                                    <div class="file-preview box sm">
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success pull-right">Create New Project</button><br>
+                    </div><br>
+                </div>
         </div>
     </div>
 
 
-    <x-backend.card>
-        <x-slot name="header">
-            @lang('Welcome :Name', ['name' => $logged_in_user->name])
-        </x-slot>
 
-        <x-slot name="body">
-
-        </x-slot>
-    </x-backend.card>
 @endsection
