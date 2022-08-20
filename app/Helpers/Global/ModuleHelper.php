19299af49c106675eeb443451630ee90f499176c
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-08-20 21:31:40 +0530

    Module Helper Functions

---
+++ app/Helpers/Global/ModuleHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+if (! function_exists('defineMenuStyle')) {
+
+    function defineMenuStyle($class,$name,$moduleName)
+    {
+        $menus =  config('menus.styles');
+        $menus[$name] = $class;
+        $themeDetails = getSetting('default_theme');
+
+        $getModuleDetails = Module::find($moduleName);
+
+        if($themeDetails == $getModuleDetails->getName())
+        {
+            Config::set('menus.styles', $menus);
+            return 'menus updated';
+        }else{
+            return 'menus not updated';
+        }
+
+
+
+    }
+}
